76cb6b19dbf8ddce37e2405bee3d777a61168834
Dorian <dorianhaxhiaj@gmail.com>
2022-06-20 00:43:52 +0200

    Added Docker File configuration for creating docker image, fixed Test error when run with mvn clean install

--- src/test/java/com/enovation/assingmentsolution/controllers/PostControllerDocumentationTest.java
+++ src/test/java/com/enovation/assingmentsolution/controllers/PostControllerDocumentationTest.java
@@ -2,6 +2,7 @@ package com.enovation.assingmentsolution.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 public class PostControllerDocumentationTest {
 
